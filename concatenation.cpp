c745fa33a8010b7a889d8779f3e43fdd666a0c38
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:41:06 +0530

    Create concatenation.cpp

---
+++ concatenation.cpp
@@ -0,0 +1,25 @@
+/*
+AAMIR AYAAN QURAISHI
+23070123002
+Aim - to print the concatenation of the following string i.e "sit pune"
+*/
+
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+    
+    string firstname = "sit"; 
+    string lastname = "pune";
+    string fullname = firstname + lastname; 
+    
+    cout << fullname << endl; 
+    return 0;
+}
+
+/*
+OUTPUT
+sitpune
+*/
