1da6f617b1e0e7d8d68af879b34a3a26a41c921f
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:42:57 +0530

    Create printing_array.cpp

---
+++ printing_array.cpp
@@ -0,0 +1,22 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//Aim - to print all the numbers present in the array.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n =5;
+    int arr[n] = {10 , 20 , 30 , 40 , 50}; 
+    for (int i = 0; i < n ; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+    return 0;
+}
+
+
+
+/*
+OUTPUT
+10 20 30 40 50
+*/
